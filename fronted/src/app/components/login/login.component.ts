f35fd299f03521b12ce3cf2272b4408d7334d646
Eleazar11 <eleazarcolop202131418@cunoc.edu.gt>
2024-10-24 17:17:29 -0600

    asd

--- fronted/src/app/components/login/login.component.ts
+++ fronted/src/app/components/login/login.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginData = {
+    email: '',
+    password: ''
+  };
 
+  constructor(private http: HttpClient) {}
+
+  onLogin() {
+    this.http.post('http://localhost:8080/api/login', this.loginData)
+      .subscribe(
+        response => {
+          console.log('Inicio de sesión exitoso', response);
+        },
+        error => {
+          console.error('Error al iniciar sesión', error);
+        }
+      );
+  }
 }
+
--- fronted/src/app/components/registro/registro.component.ts
+++ fronted/src/app/components/registro/registro.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // Importa FormsModule
 
 @Component({
   selector: 'app-registro',
-  standalone: true,
-  imports: [],
+  standalone: true, // Es un componente independiente
+  imports: [CommonModule, FormsModule], // Agrega FormsModule aquí
   templateUrl: './registro.component.html',
-  styleUrl: './registro.component.css'
+  styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent {
-
+  user = {
+    username: '',
+    password: ''
+  };
 }
